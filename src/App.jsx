2c8e6a40762df46d87e6df85461d65e09d851ef6
Alex Kydanov <a.kidanov@gmail.com>
2024-08-15 00:56:15 +0300

    correct directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Description from "./assets/components/Description/Description";
-import Options from "./assets/components/Options/Options";
-import Feedback from "./assets/components/Feedback/Feedback";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
 
 const App = () => {
   const [feedback, setFeedback] = useState(() => {
