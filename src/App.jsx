2b17e8af591974fa836f7c86cd07ebc5f958a7f8
Alex Kydanov <a.kidanov@gmail.com>
2024-08-15 10:53:09 +0300

    add Notification component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 
 const App = () => {
   const [feedback, setFeedback] = useState(() => {
@@ -52,7 +53,7 @@ const App = () => {
           positivePercentage={positivePercentage}
         />
       ) : (
-        <p>No feedback given</p>
+        <Notification />
       )}
     </>
   );
